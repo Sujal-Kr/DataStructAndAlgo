f6184f20ebd542ea43fe2fa581b0e0bb62e7c71b
Sujal-Kr <85947816+Sujal-Kr@users.noreply.github.com>
2021-08-25 16:19:22 +0530

    Update Main.java

--- PepCoding/GenericTree/Main.java
+++ PepCoding/GenericTree/Main.java
@@ -76,6 +76,80 @@ public class Main{
         }
         System.out.println(".");
     }
+    public static void linewiseorder(Node node) {
+        Queue<Node> x = new LinkedList<>();
+        Queue<Node> y = new LinkedList<>();
+        x.add(node);
+        // it follows the algo of remove print add children
+        while(!x.isEmpty()) {
+            // 1.remove the node from the queue
+            node=x.remove();
+            System.out.print(node.data+" ");
+            for(Node child:node.children){
+                y.add(child);
+            }
+            if(x.isEmpty()){
+                x=new LinkedList<>(y);
+                y.clear();
+                System.out.println();
+            }
+        }
+    }
+    public static void zigzag(Node node) {
+        Stack<Node> pr=new Stack<>();
+        Stack<Node> ch=new Stack<>();
+        pr.push(node);
+        while(!pr.isEmpty()){
+            node = pr.pop();
+            System.out.print(node.data+" ");
+            int level=1;
+            if(level%2==1){
+                for(Node child : node.children)ch.push(child);
+            }
+            else{
+                for(int i=node.children.size()-1; i>=0; i--){
+                    Node child =node.children.get(i);
+                    ch.push(child);
+                }
+            }
+            if(pr.isEmpty()){
+                level++;
+                pr=ch;
+                ch=new Stack<>();
+                System.out.println();
+            }
+        }
+    }
+    public static void mirror(Node node){
+        Queue<Node>  x = new LinkedList<>();
+        x.add(node);
+        while(!x.isEmpty()){
+            // pop the first element from the queue
+            node =x.remove();
+            // add its child 
+            int size=0;
+            for(Node child:node.children){
+                x.add(child);
+                size++;
+            }
+            // swap its child
+            // ezzy pizzy boi 
+            for(int i=0,j=size-1;i<j;i++,j--){
+                Node temp = node.children.get(i);
+                node.children.set(i, node.children.get(j));
+                node.children.set(j, temp);
+            }
+        }
+    }
+    public static void removeleaf(Node node) {
+        for(int i=node.children.size()-1; i>=0; i--){
+            Node child= node.children.get(i);
+            if(child.children.size()==0)node.children.remove(child);
+        }
+        for(Node child : node.children){
+            removeleaf(child);
+        }   
+    }
     public static void main(String[] args) {
         Node root=null;
         int[] arr={10,20,50,-1,60,-1,-1,30,70,-1,80,110,-1,120,-1,-1,90,-1,-1,40,100,-1,-1,-1};
@@ -90,11 +164,17 @@ public class Main{
                 st.push(x);
             }
         }
-        display(root);
-        System.out.println(size(root));
-        System.out.println(maximum(root));
-        System.out.println(hieght(root));
-        traversal(root);
-        levelorder(root);
+        // display(root);
+        // System.out.println(size(root));
+        // System.out.println(maximum(root));
+        // System.out.println(hieght(root));
+        // traversal(root);
+        // levelorder(root);
+        // linewiseorder(root);
+        // zigzag(root);
+        // mirror(root);
+        // linewiseorder(root);
+        removeleaf(root);
+        linewiseorder(root);
     }
 }
